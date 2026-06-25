3ce55dfa05deb0bd5767a4af9ad2b79314dd0747
Srilakshmi <srilakshmigopavarapu@gmail.com>
2026-06-25 07:10:22 +0530

    Initial commit

---
+++ age.py
@@ -0,0 +1,4 @@
+current_year = int(input("Enter the current year: "))
+birth_year = int(input("Enter the birth year: "))
+age = current_year - birth_year
+print("You are", age," years old.")
---
+++ percentage.py
@@ -0,0 +1,4 @@
+obtained = float(input("enter obtained marks: "))
+total = float(input("Enter total marks: "))
+percentage = (obtained/total)*100
+print("Pentage:",percentage)
